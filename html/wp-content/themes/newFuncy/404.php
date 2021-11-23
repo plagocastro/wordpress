204f1b72685bdf828f1257a0f20dad2af8149445
plagocastro <plagocastro@danielcastelao.org>
2021-11-23 10:58:11 +0100

    404  terminada :)

--- html/wp-content/themes/newFuncy/404.php
+++ html/wp-content/themes/newFuncy/404.php
@@ -2,8 +2,11 @@
 get_header();
 ?>
 <!--Aqui va my 404-->
-<body style="background-color: #ffe09a; text-align: center;">
-<h1>Pese a todos los intentos por completar su busqueda no se ha encontrado lo que deseaba :(</h1>
+
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css" type="text/css">
+
+
+<h1>Pese a todos los intentos por completar su busqueda<br> no se ha encontrado lo que deseaba <br>:(</h1>
 <h2>Pero mire que mono mas guapo</h2>
     <img src="<?php echo get_stylesheet_directory_uri();?>/images/mono.jpg">
 <br>
@@ -65,8 +68,31 @@ get_header();
 </body>
 
 <div id="contenedor">
-    <div id="cabecera">
-        <h1>Joala Juenos Jias</h1>
+    <div id="abosoluto" style="position: absolute; height: 300px;width: 500px; top: 500px; right: 10px">
+        <h1 style="background: #008ec2">MONKE-ARMY</h1>
+        <img src="<?php echo get_stylesheet_directory_uri();?>/images/monkeflip.gif">
+    </div>
+    <div id="abosoluto" style="position: absolute; height: 300px;width: 500px; top: 500px; left: 10px">
+        <h1 style="background: #008ec2">MONKE-ARMY</h1>
+        <img src="<?php echo get_stylesheet_directory_uri();?>/images/monkeflip.gif">
+    </div>
+    <div id="relativo" style=" position: relative;width: 400px;height: 200px">
+        <h1 style="background: #ff90e9">DISCULPE POR LAS MOLESTIAS</h1>
+        <div id="datos">
+            <p> Morir de pena<br>
+                Un chimpancé puede deprimirse y morir de pena ante la pérdida de un ser querido, como por ejemplo, la madre. Es el caso del joven Flint y su madre Flo, en Gombe.
+            </p>
+            <p> Un beso entre chimpancés<br>
+                Los chimpancés, al igual que los humanos, se dan besos, abrazos y palmadas en la espalda, se tocan las manos, se hacen cosquillas…  ¡Y hasta se ríen cuando juegan!</p>
+            <p>¿Quién es el padre de este chimpancé?<br>
+                Es difícil determinar la paternidad de los chimpancés. Recientemente es posible hacerlo gracias al análisis de ADN en muestras de excrementos.</p>
+        </div>
+    </div>
+    <div id="fixed" style="position: fixed;background: #6b94ff; bottom: 0;right: 0;width: 300px; border: 3px solid #73AD21;">
+        <h2>PASE UN PRECIOSO DIA :D</h2>
+    </div>
+    <div id="fixed" style="position: fixed;top: 0;left: 0;width: 250px; border: 10px solid #73AD21;">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/images/pandilla.gif">
     </div>
 </div>
 
